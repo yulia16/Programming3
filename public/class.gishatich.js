867968d8ed8bfe953a126c834df674398bb003db
unknown <yulia.arzumanyan.y@tumo.org>
2018-03-12 00:23:04 +0400

    optimizacvac kod

--- public/class.gishatich.js
+++ public/class.gishatich.js
@@ -1,20 +1,140 @@
-class Gishatich {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.index = 3;
-        this.multiply = 8;
-        this.energy = 2;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+// class Gishatich {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//         this.index = 3;
+//         this.multiply = 8;
+//         this.energy = 2;
+//         this.directions = [
+//             [this.x - 1, this.y - 1],
+//             [this.x, this.y - 1],
+//             [this.x + 1, this.y - 1],
+//             [this.x - 1, this.y],
+//             [this.x + 1, this.y],
+//             [this.x - 1, this.y + 1],
+//             [this.x, this.y + 1],
+//             [this.x + 1, this.y + 1]
+//         ];
+//     }
+//     stanalNorKordinatner() {
+//         this.directions = [
+//             [this.x - 1, this.y - 1],
+//             [this.x, this.y - 1],
+//             [this.x + 1, this.y - 1],
+//             [this.x - 1, this.y],
+//             [this.x + 1, this.y],
+//             [this.x - 1, this.y + 1],
+//             [this.x, this.y + 1],
+//             [this.x + 1, this.y + 1]
+//         ];
+//     }
+
+
+//     yntrelVandak(ch) {
+//         this.stanalNorKordinatner();
+//         var found = [];
+//         for (var i in this.directions) {
+//             var x = this.directions[i][0];
+//             var y = this.directions[i][1];
+//             if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+//                 if (matrix[y][x] == ch) {
+//                     found.push(this.directions[i]);
+//                 }
+//             }
+//         }
+//         return found;
+//     }
+
+//     sharjvel() {
+//         this.stanalNorKordinatner();
+//         var vandak = random(this.yntrelVandak(0));
+//         if (vandak) {
+//             matrix[this.y][this.x] = 0;
+//             matrix[vandak[1]][vandak[0]] = 3;
+//             this.x = vandak[0];
+//             this.y = vandak[1];
+//             this.multiply--;
+//         }
+//         else {
+//             var vandak = random(this.yntrelVandak(1));
+//             if (vandak) {
+//                 matrix[this.y][this.x] = 1;
+//                 var norxot = new Grass(this.y, this.x);
+//                 grassArr.push(norxot);
+//                 matrix[vandak[1]][vandak[0]] = 3;
+//                 this.x = vandak[0];
+//                 this.y = vandak[1];
+//                 this.multiply--;
+//             }
+//         }
+
+//     }
+//     bazmanal() {
+//         this.multiply++;
+//         var norVandak = random(this.yntrelVandak(0));
+//         if (this.multiply >= 10 && norVandak && this.energy >= 3) {
+//             var norGishatich = new Gishatich(norVandak[0], norVandak[1]);
+//             gishatichArr.push(norGishatich);
+//             matrix[norVandak[1]][norVandak[0]] = 3;
+//             this.multiply = 0;
+//             this.energy == 0;
+//         }
+//         else {
+//             var norVandak = random(this.yntrelVandak(1));
+//             if (this.multiply >= 10 && norVandak && this.energy >= 3) {
+//                 var norGishatich = new Gishatich(norVandak[0], norVandak[1]);
+//                 gishatichArr.push(norGishatich);
+//                 matrix[norVandak[1]][norVandak[0]] = 3;
+//                 this.multiply = 0;
+//                 this.energy == 0;
+//             }
+//         }
+//     }
+
+
+
+//     utel() {
+//         var xotaker = this.yntrelVandak(2);
+//         var norVandak = random(xotaker);
+//         if (norVandak) {
+//             matrix[this.y][this.x] = 0;
+//             matrix[norVandak[1]][norVandak[0]] = 3;
+//             this.x = norVandak[0];
+//             this.y = norVandak[1];
+//             for (var i in xotakerArr) {
+//                 if (xotakerArr[i].x == this.x && xotakerArr[i].y == this.y) {
+//                     xotakerArr.splice(i, 1);
+//                     break;
+//                 }
+//             }
+//             this.energy++;
+//         }
+
+
+//     }
+
+//     mahanal() {
+//         if (this.energy == 0 && this.multiply == 0) {
+//             for (var i in gishatichArr) {
+//                 if (gishatichArr[i].x == this.x && gishatichArr[i].y == this.y) {
+
+
+//                     gishatichArr.splice(i, 1);
+//                     matrix[this.y][this.x] = 0;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+class Gishatich extends KendaniEak {
+    constructor(x, y, index) {
+        super(x, y, index);
+        this.tariq = 0;
+        this.energy = 4;
     }
     stanalNorKordinatner() {
         this.directions = [
@@ -28,24 +148,10 @@ class Gishatich {
             [this.x + 1, this.y + 1]
         ];
     }
-
-
     yntrelVandak(ch) {
         this.stanalNorKordinatner();
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-
-                if (matrix[y][x] == ch) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+        return super.yntrelVandak(ch);
     }
-
     sharjvel() {
         this.stanalNorKordinatner();
         var vandak = random(this.yntrelVandak(0));
@@ -54,45 +160,65 @@ class Gishatich {
             matrix[vandak[1]][vandak[0]] = 3;
             this.x = vandak[0];
             this.y = vandak[1];
-            this.multiply--;
+            if (this.energy > 0)
+                this.energy--;
         }
         else {
             var vandak = random(this.yntrelVandak(1));
             if (vandak) {
-                matrix[this.y][this.x] = 1;
-                var norxot = new Grass(this.y, this.x);
-                grassArr.push(norxot);
+                matrix[this.y][this.x] = 0;
                 matrix[vandak[1]][vandak[0]] = 3;
+
                 this.x = vandak[0];
                 this.y = vandak[1];
-                this.multiply--;
+                for (var i in grassArr) {
+                    if (grassArr[i].x == this.x && grassArr[i].y == this.y) {
+                        grassArr.splice(i, 1);
+                        break;
+                    }
+                }
+                if (this.energy > 0)
+                    this.energy--;
+                this.multiply++;
             }
         }
-
     }
     bazmanal() {
-        this.multiply++;
-        var norVandak = random(this.yntrelVandak(0));
-        if (this.multiply >= 10 && norVandak && this.energy >= 3) {
-            var norGishatich = new Gishatich(norVandak[0], norVandak[1]);
-            gishatichArr.push(norGishatich);
-            matrix[norVandak[1]][norVandak[0]] = 3;
-            this.multiply = 0;
-            this.energy == 0;
-        }
-        else {
-            var norVandak = random(this.yntrelVandak(1));
-            if (this.multiply >= 10 && norVandak && this.energy >= 3) {
-                var norGishatich = new Gishatich(norVandak[0], norVandak[1]);
+        if (this.energy >= 4) {
+
+            var vandak = random(this.yntrelVandak(1));
+            if (vandak) {
+                matrix[vandak[1]][vandak[0]] = 3;
+
+                var x = vandak[0];
+                var y = vandak[1];
+
+                var norGishatich = new Gishatich(x, y);
                 gishatichArr.push(norGishatich);
-                matrix[norVandak[1]][norVandak[0]] = 3;
-                this.multiply = 0;
-                this.energy == 0;
+                for (var i in grassArr) {
+                    if (grassArr[i].x == this.x && grassArr[i].y == this.y) {
+                        grassArr.splice(i, 1);
+                        break;
+                    }
+                }
+                this.energy = 1;
+
             }
+            else {
+                var vandak = random(this.yntrelVandak(0));
+                if (vandak) {
+                    matrix[vandak[1]][vandak[0]] = 3;
+                    var x = vandak[0];
+                    var y = vandak[1];
+                    var norGishatich = new Gishatich(x, y);
+                    gishatichArr.push(norGishatich);
+                    this.energy = 1;
+
+                }
+            }
+
         }
     }
-
-
 
     utel() {
         var xotaker = this.yntrelVandak(2);
@@ -108,6 +234,7 @@ class Gishatich {
                     break;
                 }
             }
+
             this.energy++;
         }
 
@@ -115,7 +242,7 @@ class Gishatich {
     }
 
     mahanal() {
-        if (this.energy == 0 && this.multiply == 0) {
+        if (this.energy == 0 && this.multiply >= 25) {
             for (var i in gishatichArr) {
                 if (gishatichArr[i].x == this.x && gishatichArr[i].y == this.y) {
 
@@ -125,6 +252,9 @@ class Gishatich {
                     break;
                 }
             }
+            this.multiply = 0;
         }
+        console.log(this.multiply);
+        console.log(this.energy);
     }
 }
--- public/class.xot.js
+++ public/class.xot.js
@@ -40,20 +40,28 @@
 //         }
 //     }
 // }
+
+
+
+
 class Grass extends KendaniEak {
 
     bazmanal() {
-        this.multiply++;
+        // this.multiply++;
         this.direction = random(this.yntrelVandak(0));
-        if (this.multiply >= 8 && this.direction) {
+        if (this.direction) {
             var newGrass = new Grass(this.direction[0], this.direction[1], this.index);
             newGrass.parentX = this.x;
             newGrass.parentY = this.y;
             grassArr.push(newGrass);
-            matrix[this.direction[1]][this.direction[0]] = 1;
-            this.multiply = 0;
+
+            matrix[this.direction[1]][this.direction[0]] = this.index;
+
+            // this.multiply = 0;
         }
     }
 }
+
+
 
 
